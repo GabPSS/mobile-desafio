d56db07402ce56075ce8d83926c56753bc83af71
GabPSS <45743069+GabPSS@users.noreply.github.com>
2026-09-09 20:24:38 -0300

    Update da tela de cadastro de filmes

--- app/src/main/java/com/example/catlogodefilmes/view/CadastroFilmesActivity.kt
+++ app/src/main/java/com/example/catlogodefilmes/view/CadastroFilmesActivity.kt
@@ -1,16 +1,46 @@
 package com.example.catlogodefilmes.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.catlogodefilmes.R
+import com.example.catlogodefilmes.dao.FilmeDao
+import com.example.catlogodefilmes.model.Filme
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class CadastroFilmesActivity : AppCompatActivity(R.layout.activity_cadastro_filmes) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        val edtTitulo = findViewById<EditText>(R.id.edt_cadastro_titulo)
+        val edtGenero = findViewById<EditText>(R.id.edt_cadastro_genero)
+        val edtURL = findViewById<EditText>(R.id.edt_cadastro_url)
+
+        val btnCadastrar = findViewById<Button>(R.id.btn_cadastro_ok)
+
+        btnCadastrar.setOnClickListener {
+            val titulo = edtTitulo.text.toString()
+            val genero = edtGenero.text.toString()
+            val url = edtURL.text.toString()
+
+            FilmeDao.adicionar(Filme(titulo, genero, url))
+
+            Toast.makeText(this, "Filme cadastrado com sucesso!", Toast.LENGTH_SHORT).show()
+        }
+
+        val fabAvanca = findViewById<FloatingActionButton>(R.id.fab_cadastro_avanca)
+
+        fabAvanca.setOnClickListener {
+            val intent = Intent(this, ListaFilmesActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/catlogodefilmes/view/ListaFilmesActivity.kt
+++ app/src/main/java/com/example/catlogodefilmes/view/ListaFilmesActivity.kt
@@ -17,20 +17,6 @@ class ListaFilmesActivity : AppCompatActivity(R.layout.activity_lista_filmes) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val edtTitulo = findViewById<EditText>(R.id.edt_cadastro_titulo)
-        val edtGenero = findViewById<EditText>(R.id.edt_cadastro_genero)
-        val edtURL = findViewById<EditText>(R.id.edt_cadastro_url)
 
-        val btnCadastrar = findViewById<Button>(R.id.btn_cadastro_ok)
-
-        btnCadastrar.setOnClickListener {
-            val titulo = edtTitulo.text.toString()
-            val genero = edtGenero.text.toString()
-            val url = edtURL.text.toString()
-
-            FilmeDao.adicionar(Filme(titulo, genero, url))
-
-            Toast.makeText(this, "Filme cadastrado com sucesso!", Toast.LENGTH_SHORT).show()
-        }
     }
 }
