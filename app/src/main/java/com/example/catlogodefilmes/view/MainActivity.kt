bb6adca5e3911dbb8210075cde9275ce1178923c
GabPSS <45743069+GabPSS@users.noreply.github.com>
2026-09-09 20:05:11 -0300

    Criação de Telas Inicial

--- app/src/main/java/com/example/catlogodefilmes/view/MainActivity.kt
+++ app/src/main/java/com/example/catlogodefilmes/view/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.catlogodefilmes.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +15,29 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+
+        val edtLogin = findViewById<EditText>(R.id.edt_login_usuario)
+        val edtSenha = findViewById<EditText>(R.id.edt_login_senha)
+        val btnEntrar = findViewById<Button>(R.id.btn_login_entrar)
+
+
+        btnEntrar.setOnClickListener {
+            val login = edtLogin.text.toString()
+            val senha = edtSenha.text.toString()
+
+            // Login e senha Hardcoded:
+            // Login: admin
+            // Senha: password
+
+            if (login == "admin" && senha == "password") {
+                val intent = Intent(this, CadastroFilmesActivity::class.java)
+
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Usuário ou senha incorretos!", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
